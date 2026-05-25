d92e1c14d4ba6ce717e6b2d2260e0a561f5a5754
Spaceman-In-Disguise <diegoperdomosr@gmail.com>
2026-05-25 15:51:11 -0400

    base files

---
+++ proyecto_1/src/proyecto#1.cpp
@@ -0,0 +1,63 @@
+﻿#include "Engine2D.h"
+#include <iostream>
+
+class proyecto1 : public Engine2D {
+private:
+    Color colorFondo = Color(0.1f, 0.1f, 0.15f);
+    Color colorPincel = Color(1.0f, 0.0f, 0.0f);
+    bool dibujando = false;
+public:
+    proyecto1(): Engine2D(1024, 600, "Proyecto #1 - Gestion y Despliegue de Primitivas") {}
+    void setup() override {
+        clear(colorFondo);
+        std::cout << "Motor inicializado exitosamente." << std::endl;
+    }
+    // Eventos
+    void onkeyDown(int key) override {
+        if (key == GLFW_KEY_SPACE) {
+            clear(colorFondo);
+        }
+    }
+    void onMouseButtonDown(int button, double x, double y) override {
+        if (button == GLFW_MOUSE_BUTTON_LEFT) {
+            dibujando = true;
+        }
+        putPixel(x, y, colorPincel);
+    }
+    void onMouseButtonUp(int button, double x, double y) override {
+        if (button == GLFW_MOUSE_BUTTON_LEFT) {
+            dibujando = false;
+        }
+    }
+    // Evento de movimiento continuo
+    void onMouseMove(double x, double y) override {
+        if (dibujando) {
+            int ix = static_cast<int>(x);
+            int iy = static_cast<int>(y);
+            putPixel(ix, iy, colorPincel);
+        }
+    }
+    void update(float deltaTime) override {
+        // Aquí irían cosas que cambian automáticamente con el tiempo
+    }
+    void drawUI() override {
+        ImGui::Begin("Herramientas");
+        ImGui::Text("FPS: %.1f", ImGui::GetIO().Framerate);
+        ImGui::Separator();
+        float col[3] = { colorPincel.r, colorPincel.g, colorPincel.b };
+        if (ImGui::ColorEdit3("Color Pincel", col)) {
+            colorPincel.r = col[0];
+            colorPincel.g = col[1];
+            colorPincel.b = col[2];
+        }
+        ImGui::Text("Manten click izquierdo para dibujar.");
+        ImGui::Text("Presiona ESPACIO para limpiar.");
+        ImGui::End();
+    }
+};
+
+int main() {
+    proyecto1 app;
+    app.run();
+    return 0;
+}
