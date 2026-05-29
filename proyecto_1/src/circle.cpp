453d9cd2b3bf6203196d843023608054b39463fa
Spaceman-In-Disguise <diegoperdomosr@gmail.com>
2026-05-28 20:50:16 -0400

    circles working fine

---
+++ proyecto_1/src/circle.cpp
@@ -0,0 +1,55 @@
+//
+// Created by ManguitoRTX on 5/28/2026.
+//
+
+#include "circle.h"
+
+#include <cmath>
+#include <utility>
+
+Circle::Circle(Point p0, Point p1, const Color& color, PixelCallback pixelWriter)
+    : p0(p0)
+    , p1(p1)
+    , color(color)
+    , pixelWriter(std::move(pixelWriter)) {}
+
+void Circle::draw() {
+    if (!pixelWriter) {
+        return;
+    }
+
+    const int dx = p1.x - p0.x;
+    const int dy = p1.y - p0.y;
+    const int radius = static_cast<int>(std::lround(std::sqrt(static_cast<double>(dx * dx + dy * dy))));
+
+    int x = 0;
+    int y = radius;
+    int decision = 3 - 2 * radius;
+
+    auto plotSymmetricPoints = [this](int offsetX, int offsetY) {
+        pixelWriter(p0.x + offsetX, p0.y + offsetY, color);
+        pixelWriter(p0.x - offsetX, p0.y + offsetY, color);
+        pixelWriter(p0.x + offsetX, p0.y - offsetY, color);
+        pixelWriter(p0.x - offsetX, p0.y - offsetY, color);
+        pixelWriter(p0.x + offsetY, p0.y + offsetX, color);
+        pixelWriter(p0.x - offsetY, p0.y + offsetX, color);
+        pixelWriter(p0.x + offsetY, p0.y - offsetX, color);
+        pixelWriter(p0.x - offsetY, p0.y - offsetX, color);
+    };
+
+    while (y >= x) {
+        plotSymmetricPoints(x, y);
+        x++;
+
+        if (decision > 0) {
+            y--;
+            decision += 4 * (x - y) + 10;
+        } else {
+            decision += 4 * x + 6;
+        }
+    }
+}
+
+void Circle::setP1(Point new_p1) {
+    this->p1 = new_p1;
+}
---
+++ proyecto_1/src/circle.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#ifndef PROYECTO_1_CIRCLE_H
+#define PROYECTO_1_CIRCLE_H
+
+#include "Shape.h"
+#include <functional>
+
+class Circle : public Shape {
+public:
+    using PixelCallback = std::function<void(int, int, const Color&)>;
+
+    Circle(Point p0, Point p1, const Color& color, PixelCallback pixelWriter);
+
+    void draw() override;
+    void setP1(Point new_p1);
+private:
+    Point p0;
+    Point p1;
+    Color color;
+    PixelCallback pixelWriter;
+};
+
+#endif //PROYECTO_1_CIRCLE_H
